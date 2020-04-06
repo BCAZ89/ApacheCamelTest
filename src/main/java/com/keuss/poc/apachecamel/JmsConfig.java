d2f1e383faab6221cbcd33ff9787496ddfb651ec
Guillaume GALLOIS <guillaume.gallois-ext@socgen.com>
2020-04-06 15:39:21 +0200

    add pool activeMQ

--- src/main/java/com/keuss/poc/apachecamel/JmsConfig.java
+++ src/main/java/com/keuss/poc/apachecamel/JmsConfig.java
@@ -1,36 +1,46 @@
 package com.keuss.poc.apachecamel;
 
-import org.apache.activemq.spring.ActiveMQConnectionFactory;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.pool.PooledConnectionFactory;
 import org.apache.camel.component.activemq.ActiveMQComponent;
 import org.apache.camel.component.jms.JmsComponent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.connection.JmsTransactionManager;
 
-import javax.jms.ConnectionFactory;
-
 @Configuration
 public class JmsConfig {
 
+    // setup JMS connection factory
+    @Bean(destroyMethod = "stop", initMethod = "start")
+    PooledConnectionFactory poolConnectionFactory(final ActiveMQConnectionFactory jmsConnectionFactory) {
+        PooledConnectionFactory pooledConnectionFactory = new PooledConnectionFactory();
+        pooledConnectionFactory.setConnectionFactory(jmsConnectionFactory);
+        pooledConnectionFactory.setMaxConnections(8);
+        return pooledConnectionFactory;
+    }
+
     @Bean
-    ConnectionFactory connectionFactory() {
+    ActiveMQConnectionFactory jmsConnectionFactory() {
         ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
         activeMQConnectionFactory.setBrokerURL("tcp://localhost:61616");
         return activeMQConnectionFactory;
     }
 
+    // setup spring jms TX manager
     @Bean
-    public JmsTransactionManager jmsTransactionManager(final ConnectionFactory connectionFactory) {
+    public JmsTransactionManager jmsTransactionManager(final PooledConnectionFactory poolConnectionFactory) {
         JmsTransactionManager jmsTransactionManager = new JmsTransactionManager();
-        jmsTransactionManager.setConnectionFactory(connectionFactory);
+        jmsTransactionManager.setConnectionFactory(poolConnectionFactory);
         return jmsTransactionManager;
     }
 
+    // define our activemq component
     @Bean(name = "activemq")
-    public JmsComponent jmsComponent(final ConnectionFactory connectionFactory,
+    public JmsComponent jmsComponent(final PooledConnectionFactory poolConnectionFactory,
                                      final JmsTransactionManager jmsTransactionManager) {
         ActiveMQComponent activeMQComponent = new ActiveMQComponent();
-        activeMQComponent.setConnectionFactory(connectionFactory);
+        activeMQComponent.setConnectionFactory(poolConnectionFactory);
         activeMQComponent.setTransactionManager(jmsTransactionManager);
         activeMQComponent.setTransacted(true);
         activeMQComponent.setLazyCreateTransactionManager(false);
@@ -39,5 +49,4 @@ public class JmsConfig {
         return activeMQComponent;
     }
 
-    //add pooled cf !
 }
